77e25172a712fcd32556f3e0c3a37698c70ab4af
José Pekkarinen <koalinux@gmail.com>
2020-09-12 17:54:14 +0300

    Add dog_class assignment
    Signed-off-by: José Pekkarinen <koalinux@gmail.com>

---
+++ Module_1/dog_class/src/dog_class.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include "dog_class.hpp"
+
+//Definitions of class functions with some errors
+
+
+Dog::Dog(int a, std::string n) {
+    age_ = a;
+    name_ = n;
+}
+
+
+void Dog::SetAge(int a) {
+    age_ = a;
+}
+
+
+int Dog::GetAge() const {
+    return age_;
+}
+
+
+void Dog::SetName(std::string n) {
+    name_ = n;
+}
+
+
+std::string Dog::GetName() const {
+    return name_;
+}
+
+
