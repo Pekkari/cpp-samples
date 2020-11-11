debdcaa70c54a0aca3d0db4d5cd433e0db687f58
Olli Jarvinen <olli.m.jarvinen@aalto.fi>
2020-11-11 16:31:10 +0200

    First commit
    Reviewed-by: JosÃ Pekkarinen <koalinux@gmail.com>
    Signed-off-by: Olli Jarvinen <olli.m.jarvinen@aalto.fi>

--- include/items/item.hpp
+++ include/items/item.hpp
@@ -1,4 +1,17 @@
+#include <string>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics.hpp>
 
 class Item {
-
+public:
+    explicit Item(std::string& name, sf::Vector2<float> position) :
+        name_(name), position_(position) {}
+    virtual ~Item() {};
+    std::string getName() const;
+    sf::Vector2<float> getPosition() const;
+    sf::Sprite getSprite() const;
+private:
+    std::string name_;
+    sf::Vector2<float> position_;
+    sf::Sprite sprite_;
 };
