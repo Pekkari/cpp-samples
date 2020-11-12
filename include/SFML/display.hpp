24d6fdf8f72721daf5455483bd1831e481b76c9b
José Pekkarinen <koalinux@gmail.com>
2020-11-12 11:55:20 +0200

    Introduce display and input class
    This change introduces the classes for display
    input and input events. It's possible to run
    a black full screen that is running on its own
    assynchronous thread.
    
    Signed-off-by: José Pekkarinen <koalinux@gmail.com>

---
+++ include/SFML/display.hpp
@@ -0,0 +1,20 @@
+#include <future>
+#include <queue>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+#include "SFML/input.hpp"
+
+class Display {
+    std::future<int> display_listener_;
+    sf::RenderWindow window_;
+    std::queue<InputEvent> input_event_queue_;
+
+    int listen();
+public:
+    Display();
+    Display(int (&handler)());
+
+    std::future<int> GetListenerThread() { return std::move(display_listener_); }
+    std::queue<InputEvent> GetInputEventQueue() { return input_event_queue_; }
+    void Close();
+};
--- src/dungeon-crawler.cpp
+++ src/dungeon-crawler.cpp
@@ -1,5 +1,10 @@
-
+#include "SFML/display.hpp"
 
 int main(void) {
+    Display dis;
+
+    std::future<int> display_listener = dis.GetListenerThread();
+    display_listener.get();
+
     return 0;
 }
