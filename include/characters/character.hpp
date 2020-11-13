4762acad402ce004215790887e3ab50c4c72f76f
ppalmuma <pirkka.palmumaki@aalto.fi>
2020-11-13 17:54:44 +0200

    Added dummy character class implementation.
    Reviewed-by: JosÃ Pekkarinen <koalinux@gmail.com>
    Signed-off-by: Pirkka Palmumäki <pirkka.palmumaki@aalto.fi>

--- include/characters/character.hpp
+++ include/characters/character.hpp
@@ -1,3 +1,15 @@
 #include <string>
 #include <SFML/System/Vector2.hpp>
+/*
+This is a dummy implementation of the character class. This should be replaced with Garold's version.
+*/
+class Character {
 
+    public:
+        Character(std::string name, sf::Vector2<float> position) : name_(name), position_(position) {}
+    private:
+        std::string name_;
+        sf::Vector2<float> position_;
+
+
+};
