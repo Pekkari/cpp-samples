b9b5d5387ad752a745f5c8237d60e5c71e3638bb
ppalmuma <pirkka.palmumaki@aalto.fi>
2020-11-15 19:52:20 +0200

    Modified npc.hpp and door.hpp.
    Signed-off-by: Pirkka Palmumäki <pirkka.palmumaki@aalto.fi>

--- include/characters/npcs/door.hpp
+++ include/characters/npcs/door.hpp
@@ -9,8 +9,12 @@ class Door : public NPC {
         Door(std::string& name, sf::Vector2<float> position)
             : NPC(name, position) {}
 
+        bool isLocked() const;
+        bool open();
+        
 
+    private:
+        bool isLocked_;
 
-
-
+        
 };
--- include/characters/npcs/npc.hpp
+++ include/characters/npcs/npc.hpp
@@ -1,3 +1,8 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <SFML/System/Vector2.hpp>
 #include "characters/character.hpp"
 
 class NPC : public Character {
@@ -8,10 +13,33 @@ class NPC : public Character {
 
         NPC(std::string& name, sf::Vector2<float> position)
             : Character(name, position) {}
+        
+    
+   
 
 
-    // Not yet defined how the NPCs are keeping the items they are supposed to give.
+        // Get and set positions, move
+        sf::Vector2<float> getPosition() const;
+        bool setPosition(sf::Vector2<float> newPosition);
+
+        // Get and set name
+        std::string getName() const;
+        bool setName(const std::string& name);
+
+        
+
+        // Handling items, see below
+        // std::vector<Item*> getItemList() const;
+        // bool addItem(Item* item);
+        // Item* popItem(std::string itemName); // Also ITEM_TYPE may be used, tbd
+        
 
 
 
+    private:
+        sf::Vector2<float> position_;
+        // Not yet defined how the NPCs are keeping the items they are supposed to give.
+        //std::vector<Item*> items_; placeholder for Item class objects the NPC might have
+
+        
 };
